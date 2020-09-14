55602ea2c8a6dfdef8712a09f05dbd166882eb24
Inga <ijaanimets@gmail.com>
2020-09-14 16:51:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -15,7 +15,7 @@ WIN_COMBINATIONS = [
   [2, 4, 6]
    ]
    
-=begin     def won?(board)
+    def won?(board)
        
      WIN_COMBINATIONS.detect do |combo|
       
@@ -23,33 +23,6 @@ WIN_COMBINATIONS = [
         end
       end
       
-=end
-      
-      def won?(board)
-        win_index_1 = WIN_COMBINATIONS[0]
-        win_index_2 = WIN_COMBINATIONS[1]
-        win_index_3 = WIN_COMBINATIONS[2]
-        win_index_4 = WIN_COMBINATIONS[3]
-        win_index_5 = WIN_COMBINATIONS[4]
-        win_index_6 = WIN_COMBINATIONS[5]
-        win_index_7 = WIN_COMBINATIONS[6]
-        win_index_8 = WIN_COMBINATIONS[7]
-        
-          position_1 = board[win_index_1]
-          position_2 = board[win_index_2]
-          position_3 = board[win_index_3]
-          position_4 = board[win_index_4]
-          position_5 = board[win_index_5]
-          position_6 = board[win_index_6]
-          position_7 = board[win_index_7]
-          position_8 = board[win_index_8]
-          
-      if position_1 == "X" && position_2 == "X" && position_3 == "X"
-        return win_combination
-        else
-         false
-        end
-       end
    
    def full?(board)
      if board.include?(" ")
